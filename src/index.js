9934b6e51aed14d66ed502c26d62603549979d71
Александр Пикуш <pikush.oleksandr@gmail.com>
2023-07-07 18:06:18 +0300

    prepere

--- src/index.js
+++ src/index.js
@@ -2,35 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-
-const data = {
-  url: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
-  product: 'Tacos With Lime',
-  price: 10.99,
-};
 ReactDOM.createRoot(document.getElementById('root')).render(
-  const painting = (
-  <div>
-    <img src={data.url} alt={data.product} width="480" />
-    <h2>{data.product}</h2>
-    <p>Ціна: {data.price}</p>
-    <button>Замовити</button>
-  </div>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-);
-// import React from 'react';
-// import { ReactDOM } from 'react';
-
-
-
-// const painting = (
-//   <div>
-//     <img src={data.url} alt={data.product} width="480" />
-//     <h2>{data.product}</h2>
-//     <p>Ціна: {data.price}</p>
-//     <button>Замовити</button>
-//   </div>
-// );
-
-// ReactDOM.render(painting, document.querySelector('#root'));
